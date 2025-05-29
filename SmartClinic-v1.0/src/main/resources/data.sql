4d00a9b8b573d351a476ebfed3abeaa9a9d54505
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-05-29 22:00:44 +0200

    Initial project setup
    - Initialized Spring Boot project with required dependencies
    - Added MongoDB and PostgreSQL configurations
    - Configured application.properties files
    - Added .gitignore and README.md

---
+++ SmartClinic-v1.0/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (id, name, created_at, updated_at) VALUES
+                                                        (1, 'ADMIN', NOW(), NOW()),
+                                                        (2, 'DOCTOR', NOW(), NOW()),
+                                                        (3, 'RECEPTIONIST', NOW(), NOW()),
+                                                        (4, 'PATIENT', NOW(), NOW());
