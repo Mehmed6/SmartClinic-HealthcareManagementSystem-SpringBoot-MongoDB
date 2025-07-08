5676cda8f6c9e4c51b210527c901d28959010d0d
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-07-08 17:39:13 +0200

    Update register endpoint path to /api/v1/register

--- SmartClinic-v1.0/src/main/java/com/doganmehmet/app/controller/RegisterController.java
+++ SmartClinic-v1.0/src/main/java/com/doganmehmet/app/controller/RegisterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
+@RequestMapping("/api/v1/register")
 @RequiredArgsConstructor
 public class RegisterController {
     private final RegisterService m_registerService;
