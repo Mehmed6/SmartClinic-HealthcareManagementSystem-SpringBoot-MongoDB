12d1d6b087baea0273c6e5b538f61acc71283f82
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-07-19 01:32:05 +0200

    Update AppConfig.java

--- SmartClinic-v1.0/src/main/java/com/doganmehmet/app/config/AppConfig.java
+++ SmartClinic-v1.0/src/main/java/com/doganmehmet/app/config/AppConfig.java
@@ -1,23 +1,22 @@
 package com.doganmehmet.app.config;
 
+import com.doganmehmet.app.exception.ApiException;
+import com.doganmehmet.app.exception.MyError;
 import com.doganmehmet.app.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
     private final IUserRepository m_userRepository;
-
-    public AppConfig(IUserRepository userRepository)
-    {
-        m_userRepository = userRepository;
-    }
 
     @Bean
     public UserDetailsService userDetailsService()
     {
         return username -> m_userRepository.findByUsername((username))
-                .orElseThrow(() -> new RuntimeException("User not found: " + username));
+                .orElseThrow(() -> new ApiException(MyError.USER_NOT_FOUND));
     }
 }
