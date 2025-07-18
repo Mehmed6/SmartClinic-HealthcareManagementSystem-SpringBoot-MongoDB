f3fee05d4c22191b39b9221a41fb5b365fa437ab
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-07-19 01:32:15 +0200

    Update App.java

--- SmartClinic-v1.0/src/main/java/com/doganmehmet/app/App.java
+++ SmartClinic-v1.0/src/main/java/com/doganmehmet/app/App.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.doganmehmet")
 @EnableJpaRepositories(basePackages = "com.doganmehmet")
 @EntityScan(basePackages = "com.doganmehmet")
+@EnableScheduling
 public class App {
 	public static void main(String[] args)
 	{
